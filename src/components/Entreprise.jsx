3407844cc053cdafba6034cdda2e38c703936393
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2022-03-07 16:33:56 +0100

    entreprise description fix

--- src/components/Entreprise.jsx
+++ src/components/Entreprise.jsx
@@ -4,6 +4,14 @@ import "../assets/css/Entreprise.css"
 import BlueBanner from "./BlueBanner"
 export default function Entreprise() {
     const missions = ["SIRH", "Recrutement", "Human centric"]
+    const figures = [
+        { number: 2019, title: "année de création" },
+        { number: 34, title: "postes ouverts" },
+        { number: 346, title: "employés" },
+        { number: "48%", title: "d'hommes" },
+        { number: "52%", title: "de femmes" },
+        { number: 32, title: "âge moyen" },
+    ]
     return (
         <React.Fragment>
             <BlueBanner />
@@ -20,6 +28,66 @@ export default function Entreprise() {
                             ))}
                         </div>
                     </div>
+                </div>
+                <div className="entreprise-description flex-row">
+                    <div className="description flex-col">
+                        <div className="bg-white border-rounded card">
+                            <h2 className="blue">En bref</h2>
+                            <p className="paragraph">
+                                Peak People est une suite de solutions en mode
+                                Saas en management de la performance, des
+                                compétences, des appétences et du potentiel
+                                humain. Elle permet aux entreprises d’améliorer
+                                leur compétitivité en anticipant leurs besoins
+                                tout au long de leur développement permettant
+                                ainsi un accompagnement ciblé de chaque
+                                collaborateur à chaque étape de son parcours
+                                dans l’organisation, et ce dès le recrutement.
+                                <br />
+                                <br />
+                                Plus d'informations sur : <br />
+                                <a
+                                    href="https://www.peakpeople-hr.com/fr/"
+                                    target="_blank"
+                                >
+                                    https://www.peakpeople-hr.com/fr/
+                                </a>
+                            </p>
+                        </div>
+                        <div className="bg-white border-rounded card flex-column">
+                            <h2 className="blue ">Quelques chiffres</h2>
+                            <div className="figures flex-row">
+                                <div className="numbers flex-column">
+                                    {figures.map((figure) => (
+                                        <span className="number purple">
+                                            {figure.number}
+                                        </span>
+                                    ))}
+                                </div>
+                                {/* <div className="vl"></div> */}
+                                <div className="titles flex-column">
+                                    {figures.map((figure) => (
+                                        <span className="title">
+                                            {figure.title}
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="bg-white border-rounded card blue">
+                            <h2>Suivez-nous!</h2>
+                            <span>
+                                <i class="bi bi-twitter nav-icon"></i>
+                            </span>
+                            <span>
+                                <i class="bi bi-linkedin nav-icon"></i>
+                            </span>
+                            <span>
+                                <i class="bi bi-facebook nav-icon"></i>
+                            </span>
+                        </div>
+                    </div>
+                    <div className="media flex-col"></div>
                 </div>
             </div>
         </React.Fragment>
