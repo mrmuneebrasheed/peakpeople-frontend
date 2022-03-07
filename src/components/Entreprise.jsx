ace4d37ff8ad1cec788b58b3186e742e3f5af9e5
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2022-03-07 11:14:04 +0100

    favicon added

--- src/components/Entreprise.jsx
+++ src/components/Entreprise.jsx
@@ -1,11 +1,27 @@
 import React from "react"
+import logo from "../assets/img/logo.png"
 import "../assets/css/Entreprise.css"
 import BlueBanner from "./BlueBanner"
 export default function Entreprise() {
+    const missions = ["SIRH", "Recrutement", "Human centric"]
     return (
         <React.Fragment>
             <BlueBanner />
-            <div className="entreprise-page"></div>
+            <div className="entreprise-page">
+                <div className="entreprise-title flex-row">
+                    <div className="logo-div">
+                        <img className="logo" src={logo} alt="logo" />
+                    </div>
+                    <div className="flex-col">
+                        <h1 className="title flex-row">Peak People</h1>
+                        <div className="missions">
+                            {missions.map((mission) => (
+                                <span className="mission">{mission}</span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     )
 }
