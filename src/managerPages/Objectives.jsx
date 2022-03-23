926ab35f1ac999c7c4238f25bd1eb201a0e09dab
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2022-03-23 14:50:20 +0100

    title type inputs and profiles added in objectives page

--- src/managerPages/Objectives.jsx
+++ src/managerPages/Objectives.jsx
@@ -1,30 +1,83 @@
 import React, { useState } from "react"
 import Calender from "../components/Calender"
+import ProfileCardSmall from "../components/ProfileCardSmall"
 import "./Objectives.css"
 
 export default function Objectives() {
     const [objectives, setObjectives] = useState("")
+    const profilesToShow = [
+        {
+            id: "p1",
+            firstName: "Muneeb",
+            lastName: "Rasheed",
+            job: "Full Stack Developer",
+            role: "Manager",
+            enterprise: "Peak People",
+        },
+        {
+            id: "p2",
+            firstName: "Muneeb",
+            lastName: "Rasheed",
+            job: "Full Stack Developer",
+            role: "Manager",
+            enterprise: "Peak People",
+        },
+    ]
     return (
         <div className="objectives-page">
-            <h2>Objectifs</h2>
+            <h2 className="pink text-center">Envoyer un objectif</h2>
+            <input
+                className="title-input"
+                type="text"
+                id="objective-title"
+                placeholder="Titre d'Objectif"
+            />
+            <div className="objective-type flex-row justify-evenly align-center">
+                <span className="label pink">Type d'Objectif</span>
+                <select className="filter-option" id="objective-type">
+                    <option className="filter-option" value="analytics">
+                        Analytics
+                    </option>
+                    <option className="filter-option" value="recrutement">
+                        Recrutements
+                    </option>
+                    <option className="filter-option" value="management">
+                        Management
+                    </option>
+                    <option className="filter-option" value="relationship">
+                        Relationship
+                    </option>
+                </select>
+            </div>
             <textarea
                 onChange={(e) => setObjectives(e.target.value)}
-                className="objectives-input"
-                placeholder="Objectifs"
+                className="objective-detail"
+                placeholder="Contenu d'Objectif"
                 name="objectives"
                 id="objectives"
                 cols="170"
                 rows="10"
             ></textarea>
             <Calender template="month" objective={true} />
+            <h2 className="pink text-center">Envoyer à</h2>
+            <div className="profiles flex-row">
+                {profilesToShow?.map((profile) => (
+                    <ProfileCardSmall
+                        key={profile.id}
+                        firstName={profile.firstName}
+                        lastName={profile.lastName}
+                        job={profile.job}
+                        role={profile.role}
+                        enterprise={profile.enterprise}
+                    />
+                ))}
+            </div>
             <span
                 className="pink-button send-button"
                 onClick={() => console.log(objectives)}
             >
                 Envoyer
             </span>
-            <h2>Suivi des Coaching</h2>
-            <div className="coaching"></div>
         </div>
     )
 }
