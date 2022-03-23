6f2c9566256a2f2cceaff64e2f21d6ccbe684d3b
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2022-03-23 16:49:37 +0100

    candidature Profile doing

---
+++ src/components/CandidatureProfile.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import logo from "../assets/img/logo.png"
+import moment from "moment"
+import "./CandidatureProfile.css"
+
+export default function CandidatureProfile({ candidate, date }) {
+    const dateToDisplay = moment(date).format("DD/MM/YY")
+    return (
+        <div className="candidature-profile flex-row">
+            <div className="logo flex-column">
+                <img src={logo} alt="logo" className="image" />
+            </div>
+            <div className="flex-column">
+                <div className="name">{`${candidate.firstName} ${candidate.lastName}`}</div>
+                <div className="date">{`Interne: ${dateToDisplay}`}</div>
+            </div>
+        </div>
+    )
+}
