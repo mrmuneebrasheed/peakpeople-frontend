b985039c9a2453e2c577559e41d0875271688e81
Muneeb Rasheed <mr.muneebrasheed@gmail.com>
2022-03-29 14:47:49 +0200

    skills and candidate scoring description added in variable arrays

---
+++ src/redux/scoring.js
@@ -0,0 +1,14 @@
+const candidateScoring = [
+    { score: 1, description: "WRONG FIT" },
+    { score: 2, description: "POTENTIAL" },
+    { score: 3, description: "POTENTIAL TO BECOME TALENT" },
+    { score: 4, description: "TALENT" },
+]
+const skillScoring = [
+    { score: 1, description: "DEBUTANT" },
+    { score: 2, description: "CONNAISSANCE" },
+    { score: 3, description: "PRATIQUE" },
+    { score: 4, description: "COURANT" },
+    { score: 5, description: "MAITRE" },
+]
+export default { candidateScoring, skillScoring }
